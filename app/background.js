2d83deb87818c8394c8a66839d856ea84eec134a
Silvio Henrique <silviohfc@gmail.com>
2020-10-31 10:34:17 -0300

    Added player controls feature

--- app/background.js
+++ app/background.js
@@ -22,7 +22,6 @@ const Radio = {
         xhr.send()
 
     },
-
     getAllStations(request, sender, sendResponse) {
         
         if (request.name == "all_stations") {
@@ -36,7 +35,6 @@ const Radio = {
 
         return true
     },
-
     getCurrentStation(request, sender, sendResponse) {
         
         if (request.name == "current_station") {
@@ -47,17 +45,66 @@ const Radio = {
 
         return true
     },
+    getPlayingStatus(request, sender, sendResponse) {
+        
+        if (request.name == "playing_status") {
+            console.log(`getPlayingStatus(): Getting the current playing status. [${Radio.playing}]`)
+            
+            sendResponse(Radio.playing)
+        }
+
+        return true
+    },
+    getVolume(request, sender, sendResponse) {
+        
+        if (request.name == "volume") {
+            console.log(`getVolume(): Getting the current volume. [${Radio.volume}]`)
+            
+            sendResponse(Radio.volume)
+        }
+
+        return true
+    },
 
     setCurrentStation(request, sender, sendResponse) {
 
         if (request.name == "update_station") {
-            console.log(`getCurrentStation(): Setting new selected Station. [${request.server}]`)
+            console.log(`setCurrentStation(): Setting new selected Station. [${request.server}]`)
             Radio.current_station = request.server
 
+            Radio.source.src = Radio.current_station
             sendResponse('success')
         }
 
         return true
+    },
+    setPlayingStatus(request, sender, sendResponse) {
+
+        if (request.name == "update_playing_status") {
+            console.log(`setPlayingStatus(): Setting new playing status. [${request.status}]`)
+            
+            Radio.playing = request.status
+            
+            if (Radio.playing == true) {
+                Radio.source.play()
+            } else {
+                Radio.source.pause()
+            }
+
+            sendResponse('Success')
+        }
+
+        return true
+    },
+    setVolume(request, sender, sendResponse) {
+        if (request.name == "update_volume") {
+            console.log(`setVolume(): Setting new volume. [${request.volume}]`)
+            
+            Radio.volume = request.volume
+            Radio.source.volume = request.volume / 100
+
+            sendResponse('Success')
+        }
     }
 
 }
@@ -70,7 +117,12 @@ const Chrome = {
 
 Chrome.listen(Radio.getAllStations)
 Chrome.listen(Radio.getCurrentStation)
+Chrome.listen(Radio.getPlayingStatus)
+Chrome.listen(Radio.getVolume)
+
 Chrome.listen(Radio.setCurrentStation)
+Chrome.listen(Radio.setPlayingStatus)
+Chrome.listen(Radio.setVolume)
 
 document.querySelector('body').appendChild(Radio.source)
 
--- public/scripts.js
+++ public/scripts.js
@@ -1,10 +1,17 @@
 const Popup = {
     selectElement: document.querySelector('#server'),
     placeholderOption: document.querySelector('#placeholder'),
-    stationsCache: [],
+    playerElement: document.querySelector('.player-control'),
+
+    play_pause_button: document.querySelector('.player-control i'),
+    volume_slider: document.querySelector('#volume'),
+    loader: document.querySelector('.loader'),
 
     initialize() {
+        Popup.loading(true)
+        Popup.hidePlayerElement()
         Popup.requestAllStations()
+        Popup.requestVolume()
     },
 
     requestAllStations() {
@@ -14,14 +21,11 @@ const Popup = {
                 const option = Popup.createStationElement(station)
                 Popup.selectElement.appendChild(option)
                 Popup.selectElement.style.display = "flex"
-
-                Popup.stationsCache.push(station)
             })
 
             Popup.requestCurrentStation()
         })
     },
-
     requestCurrentStation() {
         chrome.runtime.sendMessage({type: 'GET', name: 'current_station'}, station => {
             const selectedStation = Array.from(Popup.selectElement.childNodes).filter(option => option.value == station)
@@ -29,13 +33,49 @@ const Popup = {
             if (selectedStation) {
                 const selectedStationElement = document.querySelector(`option[value="${selectedStation[0].value}"]`)
                 selectedStationElement.setAttribute('selected', true)
+
+                Popup.loading(false)
+                Popup.showPlayerElement()
+                Popup.requestPlayingStatus()
             }
+        })
+    },
+    requestPlayingStatus() {
+        chrome.runtime.sendMessage({type: 'GET', name: 'playing_status'}, status => {
+            if (status == true) {
+                Popup.play_pause_button.classList.remove('fa-play')
+                Popup.play_pause_button.classList.add('fa-pause')
+            } else {
+                Popup.play_pause_button.classList.remove('fa-pause')
+                Popup.play_pause_button.classList.add('fa-play')
+            }
+        })
+    },
+    requestVolume() {
+        chrome.runtime.sendMessage({type: 'GET', name: 'volume'}, value => {
+            Popup.volume_slider.value = value
         })
     },
 
     updateSelectedStation(station) {
+        Popup.updatePlayingStatus(false)
+        Popup.hidePlayerElement()
+        Popup.loading(true)
+
         const { value: server } = station
-        chrome.runtime.sendMessage({type: 'PUT', name: 'update_station', server})
+        chrome.runtime.sendMessage({type: 'PUT', name: 'update_station', server}, status => {
+            setTimeout(() => {
+                Popup.requestPlayingStatus()
+                Popup.showPlayerElement()
+                Popup.loading(false)
+            }, 1000)
+        })
+    },
+    updatePlayingStatus(status) {
+        chrome.runtime.sendMessage({type: 'PUT', name: 'update_playing_status', status})
+    },
+    updateVolume(volume) {
+        chrome.runtime.sendMessage({type: 'PUT', name: 'update_volume', volume})
     },
 
 
@@ -46,6 +86,36 @@ const Popup = {
         option.innerHTML = station.name
 
         return option
+    },
+
+    showPlayerElement() {
+        Popup.playerElement.style.display = "flex"
+    },
+
+    hidePlayerElement() {
+        Popup.playerElement.style.display = "none"
+    },
+
+    playPauseButtonEvent(button) {
+        if (button.classList.contains('fa-play')) {
+            button.classList.remove('fa-play')
+            button.classList.add('fa-pause')
+
+            Popup.updatePlayingStatus(true)
+        } else {
+            button.classList.remove('fa-pause')
+            button.classList.add('fa-play')
+
+            Popup.updatePlayingStatus(false)
+        }
+    },
+
+    loading(status) {
+        if (status == true) {
+            Popup.loader.style.display = "block"
+        } else {
+            Popup.loader.style.display = "none"
+        }
     }
 
 }
@@ -54,5 +124,5 @@ Popup.initialize()
 
 /* --------------------------------- EVENTS --------------------------------- */
 document.querySelector('#server').addEventListener("change", event => Popup.updateSelectedStation(event.target))
-// document.querySelector('.player-control i').addEventListener("click", () => Radio.playPause())
-// document.querySelector('.player-control input').addEventListener("change", event => Radio.changeVolume(event))
+document.querySelector('.player-control i').addEventListener("click", event => Popup.playPauseButtonEvent(event.target))
+document.querySelector('.player-control input').addEventListener("change", event => Popup.updateVolume(event.target.value))
